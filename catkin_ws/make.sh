e704bcc431c4b5c64abb53929e4eb76a061e97f1
Chris McKinney <ntd5@tachibanatech.com>
2017-11-30 19:19:05 -0500

    YouCompleteMe completion

---
+++ catkin_ws/make.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd "$(dirname "$0")"
+catkin_make -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
---
+++ catkin_ws/src/setpoint_test/src/setpoint_test_node.cpp
@@ -0,0 +1,5 @@
+#include <ros/ros.h>
+
+int main(int argc, char **argv) {
+  ROS_INFO("It's time for ROS!");
+}
