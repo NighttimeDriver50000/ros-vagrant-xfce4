8ec9afb6cd16abb994f295c7be848aee36fac6b8
Chris McKinney <ntd5@tachibanatech.com>
2018-10-31 03:19:42 -0400

    Implemented EKF localization, starting naive SLAM

---
+++ catkin_ws/Session.vim
@@ -0,0 +1,3 @@
+edit ./src/mckinnc_ekf/src/ekf.cpp
+vsplit ./src/mckinnc_ekf/include/mckinnc_ekf/ekf.hpp
+split ./src/mckinnc_ekf/include/mckinnc_ekf/common.hpp
---
+++ catkin_ws/src/mckinnc_ekf/include/mckinnc_ekf/naive_slam.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "mckinnc_ekf/common.hpp"
+#include "mckinnc_ekf/ekf.hpp"
+
+#include <vector>
+
+namespace mckinnc_ekf {
+
+class NaiveSLAM {
+  public:
+    NaiveSLAM(const Covariance& modelCovariance,
+        const Observation& observationVariance,
+        double newLandmarkUncertaintyThreshold);
+
+    void run(const Control& control,
+        const std::vector<Observation>& observations);
+
+    //! \see EKF::mean()
+    const Pose& mean() { return ekf_.mean(); }
+    //! \see EKF::covariance()
+    const Covariance& covariance() { return ekf_.covariance(); }
+    //! Current set of map landmarks.
+    const std::vector<Landmark>& landmarks() { return landmarks_; }
+
+  protected:
+    EKF ekf_;
+    std::vector<Landmark> landmarks_;
+    double newLandmarkUncertaintyThreshold_;
+};
+
+} // namespace
